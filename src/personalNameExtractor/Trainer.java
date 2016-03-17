d60f137a9ec4000abcde15d57ac99e1b05a8428a
tpressle <tpres008@odu.edu>
2016-03-17 14:19:10 -0400

    Added the "addIdentifiers" function to the trainer interface

--- src/personalNameExtractor/Trainer.java
+++ src/personalNameExtractor/Trainer.java
@@ -8,5 +8,10 @@ public class Trainer {
 	 * @param inputText
 	 */
 	public String tokenize(String trainingMaterial){};
-
+	/**
+	 * Uses the tokenized text to convert the text into a form using identifiers 
+	 * which can be used to train the learning machine
+	 * @param tokenizedText
+	 */
+	public String addIdentifiers(String tokenizedText){};
 }
