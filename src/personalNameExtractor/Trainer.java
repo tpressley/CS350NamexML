2c6b82462b7279cdcd1f4539d868c3843cb39292
tpressle <tpres008@odu.edu>
2016-03-17 14:17:32 -0400

    *Created the personalNameExplorer package *Created the Trainer Interface *Added the tokenize function to the Trainer Interface

---
+++ src/personalNameExtractor/Trainer.java
@@ -0,0 +1,12 @@
+package personalNameExtractor;
+
+public class Trainer {
+	
+	/**
+	 * Takes the initial trainingMaterial and converts it into a tokenized form
+	 * and outputs the tokenized text
+	 * @param inputText
+	 */
+	public String tokenize(String trainingMaterial){};
+
+}
