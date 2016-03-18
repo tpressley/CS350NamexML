340dafb28ae2976f4e94eddc2a574544edb5575d
jbarr <jbarr@cs.odu.edu>
2016-03-18 13:15:57 -0400

    Updated library imports to include Weka

--- src/personalNameExtractor/TestTrainer.java
+++ src/personalNameExtractor/TestTrainer.java
@@ -1,4 +1,8 @@
 package personalNameExtractor;
+//import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 import static org.junit.Assert.*;
 
--- src/personalNameExtractor/Trainer.java
+++ src/personalNameExtractor/Trainer.java
@@ -1,4 +1,8 @@
 package personalNameExtractor;
+//import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class Trainer {
 
