6f8269cff39636b004c3f8dd10563b4e8bc4c14d
tpressle <tpres008@odu.edu>
2016-03-31 15:02:27 -0400

    Created the Token class

---
+++ src/main/java/edu/odu/cs/cs350/namex/Token.java
@@ -0,0 +1,17 @@
+package edu.odu.cs.cs350.namex;
+
+public class Token {
+	private boolean capitalized;
+	private boolean killWord;
+	private boolean article;
+	
+	public Token()
+	{
+		
+	}
+	
+	public Token(String text)
+	{
+		
+	}
+}
