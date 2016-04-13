3b9fe0bc86842c416a5219dfe60a26c1c1820ed5
jbarr <jbarr@cs.odu.edu>
2016-04-13 19:20:32 -0400

    jenkins build test#1

--- src/main/java/edu/odu/cs/cs350/namex/TrainingData.java
+++ src/main/java/edu/odu/cs/cs350/namex/TrainingData.java
@@ -1,6 +1,8 @@
 package edu.odu.cs.cs350.namex;
 //This does nothing, consider removing. 
 
+
+
 //this is a change for the sake of jenkins build seriously work alreadyjjjj
 public class TrainingData {
 
