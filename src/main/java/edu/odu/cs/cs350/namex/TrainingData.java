a245b2b45bf84e480819c7e7ac1696a5702bfc66
jbarr <jbarr@cs.odu.edu>
2016-04-13 17:56:03 -0400

    webhook test #7

--- src/main/java/edu/odu/cs/cs350/namex/TrainingData.java
+++ src/main/java/edu/odu/cs/cs350/namex/TrainingData.java
@@ -1,7 +1,7 @@
 package edu.odu.cs.cs350.namex;
 //This does nothing, consider removing. 
 
-//this is a change for the sake of jenkins build seriously work already
+//this is a change for the sake of jenkins build seriously work alreadyjjjj
 public class TrainingData {
 
 }
