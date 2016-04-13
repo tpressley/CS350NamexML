6f2cf74762d09559edf102218f6fc1087609fdc2
jbarr <jbarr@cs.odu.edu>
2016-04-13 19:27:44 -0400

    removing extraneous comments

--- src/main/java/edu/odu/cs/cs350/namex/TrainingData.java
+++ src/main/java/edu/odu/cs/cs350/namex/TrainingData.java
@@ -1,9 +1,5 @@
 package edu.odu.cs.cs350.namex;
-//This does nothing, consider removing. 
 
-
-//blah blah blah yakkity smakkity
-//this is a change for the sake of jenkins build seriously work alreadyjjjj
 public class TrainingData {
 
 }
