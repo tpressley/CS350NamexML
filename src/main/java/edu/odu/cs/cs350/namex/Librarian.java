a7ff0530a93247dacd42f6b3b872eb111832ed2e
addyb_000 <addyb_000@Addicuss>
2016-04-16 16:44:56 -0400

    .

--- src/main/java/edu/odu/cs/cs350/namex/Librarian.java
+++ src/main/java/edu/odu/cs/cs350/namex/Librarian.java
@@ -26,55 +26,9 @@ public class Librarian {
 		 * for (String s: args) { System.out.println(s); }
 		 */
 
-		if (args.length == 0) {
-			System.out.println("Hello World!");
-		}
-
-		// if CLI contains one argument that is a textBlock
-		else if (args.length == 1) {
-			System.out.println(args[0]);
-		}
-
-		// if CLI contains two arguments
-		else if (args.length == 2) {
-		  
-		  
-		  
-			System.out.println("2 Arguments");
-
-			String inputFileName = args[0];
-			String outputFileName = args[1];
-
-			Trainer trainer = new Trainer();
-
-			ArrayList<Token> tokens = new ArrayList<Token>();
-
-			/*
-			 * ArrayList<TextBlock> textBlocks = new ArrayList<TextBlock>();
-			 * 
-			 * HashSet<String> fileLines = Librarian.importFile(inputFileName);
-			 * for (String line : fileLines) {
-			 * textBlocks.addAll(Librarian.separateNER(line)); }
-			 */
-
-			ArrayList<TextBlock> textBlocks = importFile(inputFileName);
-
-			for (TextBlock textBlock : textBlocks) {
-				tokens.addAll(trainer.tokenize(textBlock.getTextBlock()));
-			}
-
-			// System.out.println("Imported File Successfully!");
-
-			try {
-//				trainer.prepareData(inputFileName, outputFileName, false);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 
 		// Generate ARFF Training Data
-		else if (args.length == 3) {
+		 if (args.length == 3) {
 			if (args[0].equalsIgnoreCase("train")) {
 			  
 			  
@@ -88,6 +42,7 @@ public class Librarian {
 	      String inputFileName = args[1];
 	      String outputFileName = args[2];
 	      ArrayList<String> textFile = new ArrayList<String>();
+	      ArrayList<Token> tokens = new ArrayList<Token>();
 	      Trainer trainer = new Trainer();
 	      System.out.println();
 	        Scanner sc = new Scanner(new File(args[1]));
@@ -95,16 +50,22 @@ public class Librarian {
 	            String s = sc.next();
 	            String[] wordsplit =s.split("[\\p{Punct}\\s]+");
 	            for (int i = 0 ; i<wordsplit.length; i++)
+	            {
+	              if (wordsplit[i].trim().length() > 0)
+	              {
 	            textFile.add(wordsplit[i]);
-	            
-	            
+	           // tokens.add((trainer.getFeatures((wordsplit[i]));
+	            }
+	            }  
 	        }
 	        for (int i=0; i< textFile.size();i++)
 	        {
 	          System.out.println(textFile.get(i));
 	        }
 	      
-	      
+	        
+	        
+	        
 	      
 	       inputFileName = args[1];
 	      
@@ -120,50 +81,6 @@ public class Librarian {
 	
 	
 
-	// imports a file from filePath and returns the line values as a
-	// HashSet<String>
-	// public static HashSet<String> importFile(String filePath)
-	public static ArrayList<TextBlock> importFile(String filePath) {
-		File file = new File(filePath);
-		Scanner s;
-
-		ArrayList<TextBlock> textBlocks = new ArrayList<TextBlock>();
-
-		try {
-			s = new Scanner(file);
-
-			while (s.hasNext()) {
-				textBlocks.addAll(detectNERTag(s.nextLine()));
-			}
-			s.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		return textBlocks;
-	}
-
-	// imports a file from filePath and returns the line values as a
-	// HashSet<String>
-	public static HashSet<TextBlock> importFileHash(String filePath) {
-		File file = new File(filePath);
-		Scanner s;
-
-		HashSet<TextBlock> textBlocks = new HashSet<TextBlock>();
-
-		try {
-			s = new Scanner(file);
-
-			while (s.hasNext()) {
-				textBlocks.addAll(detectNERTag(s.nextLine()));
-			}
-			s.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		return textBlocks;
-	}
 
 
 
@@ -172,22 +89,7 @@ public class Librarian {
   // Status - Completed
   // CLI processes each <NER> block separately via Extractor
   // interface. (A, maybe L)
-  public static ArrayList<TextBlock> detectNERTag(String input) {
-    
-    
-    
-  	String[] tbs = input.split("<NER>|</NER>");
-  
-  	ArrayList<TextBlock> textBlocks = new ArrayList<TextBlock>();
-  
-  	for (String tb : tbs) {
-  		if (!tb.equals("")) {
-  			textBlocks.add(new TextBlock(tb));
-  		}
-  	}
-  
-  	return textBlocks;
-  }
+
 
 
 
