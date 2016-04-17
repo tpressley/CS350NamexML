616e5f26ee824bbf3f07a4bceb7cae55b9aa0f80
cchey <cchey001@odu.edu>
2016-04-17 16:44:55 -0400

    writing tests in TestToken.java & added TestSystem.java (Tristan)

--- src/test/java/edu/odu/cs/cs350/namex/TestToken.java
+++ src/test/java/edu/odu/cs/cs350/namex/TestToken.java
@@ -227,6 +227,165 @@ public class TestToken {
 	}
 	
 	
+	
+	@Test
+	/**
+	 * test for public int getPosition()
+	 */
+	public void testGetPosition(){
+		
+		Token tok = new Token("testLex" , 0);
+		Token tok2 = new Token("testLex2" , 1);
+		Token tok3 = new Token("testLex3" , 4);
+		
+		assertEquals(0 , tok.getPosition());
+		assertEquals(1 , tok2.getPosition());
+		assertEquals(4 , tok3.getPosition());
+	}
+	
+	
+	@Test
+	/**
+	 * test for public void setPosition(int position)
+	 */
+	public void testSetPosition(){
+		Token tok = new Token();
+		Token tok2 = new Token("test");
+		Token tok3 = new Token("test2" , 0);
+		Token tok4 = new Token("test3" , 2);
+		
+		assertEquals(0, tok.getPosition());
+		assertEquals(0, tok2.getPosition());
+		assertEquals(0, tok3.getPosition());
+		assertEquals(2, tok4.getPosition());
+		
+		tok3.setPosition(3);
+		assertEquals(3 , tok3.getPosition());
+		
+		tok4.setPosition(0);
+		assertEquals(0 , tok4.getPosition());
+	}
+	
+	
+	@Test
+	/**
+	 * test for public String getLexeme()
+	 */
+	public void testGetLexeme(){
+		Token tok = new Token();
+		Token tok2 = new Token("test");
+		
+		assertEquals("" , tok.getLexeme());
+		assertEquals("test" , tok2.getLexeme());
+	}
+	
+	
+	@Test
+	/**
+	 * test for public void setLexeme(String lexeme)
+	 */
+	public void testSetLexeme(){
+		Token tok = new Token();
+		Token tok2 = new Token("test");
+		
+		tok.setLexeme("new");
+		assertEquals("new" , tok.getLexeme());
+		
+		tok2.setLexeme("next");
+		assertEquals("next" , tok2.getLexeme());
+		
+		tok2.setLexeme("");
+		assertEquals("" , tok2.getLexeme());
+	}
+	
+	
+	@Test
+	/**
+	 * test for public String getLexical() 
+	 */
+	public void testGetLexical(){
+		
+		Token tok = new Token();
+		Token tok2 = new Token("hello");
+		Token tok3 = new Token("John");
+		Token tok4 = new Token("!");
+		Token tok5 = new Token("P");
+		
+		assertEquals("" , tok.getLexical());
+		
+		tok2.setLexical("other");
+		assertEquals("other" , tok2.getLexical());
+		
+		tok3.setLexical("capitalized");
+		assertEquals("capitalized" , tok3.getLexical());
+		
+		tok4.setLexical("punct");
+		assertEquals("punct" , tok4.getLexical());
+		
+		tok5.setLexical("CapLetter");
+		assertEquals("CapLetter" , tok5.getLexical());
+	}
+
+	
+	
+	@Test
+	/**
+	 * test for public void setLexical(String lexical)
+	 */
+	public void testSetLexical(){
+		Token tok = new Token("no"); //other
+		Token tok2 = new Token("H"); //CapLetter
+		Token tok3 = new Token(","); //punct
+		Token tok4 = new Token("School"); //capitalized
+
+		tok.setLexical("other");
+		assertEquals("other" , tok.getLexical());
+
+		tok2.setLexical("CapLetter");
+		assertEquals("CapLetter" , tok2.getLexical());
+		
+		tok3.setLexical("punct");
+		assertEquals("punct" , tok3.getLexical());
+		
+		tok4.setLexical("capitalized");
+		assertEquals("capitalized" , tok4.getLexical());
+	}
+	
+	
+	@Test
+	/**
+	 * test for public String getPartOfSpeech()
+	 */
+	public void testGetPartOfSpeech(){
+		Token t1 = new Token("an");
+		Token t2 = new Token(".");
+		Token t3 = new Token("and");
+		Token t4 = new Token("bathroom");
+		Token t5 = new Token("H");
+		Token t6 = new Token("Sally");
+		Token t7 = new Token();
+		
+		t1.setLexical("other");
+		t1.setPartOfSpeech("article");
+		assertEquals("article" , t1.getPartOfSpeech());
+		
+		t2.setLexical("punct");
+		t2.setPartOfSpeech("period");
+		assertEquals("period", t2.getPartOfSpeech());
+		
+		t3.setLexical("other");
+		t3.setPartOfSpeech("conjunction");
+		assertEquals("conjunction", t3.getPartOfSpeech());
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	// User Story #850
 	// Status - Completed
 	// Tokens converted into a set of features (T)
