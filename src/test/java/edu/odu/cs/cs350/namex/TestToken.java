dfbec7c102a56698aac7a60fa09bb29aa2306fff
cchey <cchey001@odu.edu>
2016-04-17 17:34:54 -0400

    writing more tests in TestToken.java

--- src/test/java/edu/odu/cs/cs350/namex/TestToken.java
+++ src/test/java/edu/odu/cs/cs350/namex/TestToken.java
@@ -333,6 +333,17 @@ public class TestToken {
 	 * test for public void setLexical(String lexical)
 	 */
 	public void testSetLexical(){
+		
+		/*
+		 * Lexical features	
+				-   number			
+				-   punctuation mark; "punct"			
+				-   single capitalized letter; "CapLetter"				
+				-   capitalized word (of more than one letter); "capitalized"						
+				-   ALL-CAPS word (of more than one letter)						
+				-   Line feed / new line						
+				-   Null/nothing
+		 */
 		Token tok = new Token("no"); //other
 		Token tok2 = new Token("H"); //CapLetter
 		Token tok3 = new Token(","); //punct
@@ -357,6 +368,17 @@ public class TestToken {
 	 * test for public String getPartOfSpeech()
 	 */
 	public void testGetPartOfSpeech(){
+		/*
+		 * Parts of Speech
+
+				 - Articles (“a”, “an”, “the”)			
+				 - Conjunction, specifically “and”				
+				 - Period					
+				 - Comma				
+				 - Hyphen
+				 - other
+		 */
+		
 		Token t1 = new Token("an");
 		Token t2 = new Token(".");
 		Token t3 = new Token("and");
@@ -364,27 +386,351 @@ public class TestToken {
 		Token t5 = new Token("H");
 		Token t6 = new Token("Sally");
 		Token t7 = new Token();
+		Token t8 = new Token(",");
+		Token t9 = new Token("-");
 		
 		t1.setLexical("other");
 		t1.setPartOfSpeech("article");
-		assertEquals("article" , t1.getPartOfSpeech());
+		assertEquals(0, ("article").compareToIgnoreCase(t1.getPartOfSpeech()));
 		
 		t2.setLexical("punct");
 		t2.setPartOfSpeech("period");
-		assertEquals("period", t2.getPartOfSpeech());
+		assertEquals(0, ("period").compareToIgnoreCase(t2.getPartOfSpeech()));
 		
 		t3.setLexical("other");
 		t3.setPartOfSpeech("conjunction");
-		assertEquals("conjunction", t3.getPartOfSpeech());
+		assertEquals(0, ("conjunction").compareToIgnoreCase(t3.getPartOfSpeech()));
+		
+		t4.setLexical("other");
+		t4.setPartOfSpeech("other");
+		assertEquals(0, ("other").compareToIgnoreCase(t4.getPartOfSpeech()));
+		
+		t5.setLexical("CapLetter");
+		t5.setPartOfSpeech("other");
+		assertEquals(0, ("other").compareToIgnoreCase(t5.getPartOfSpeech()));
+		
+		t6.setLexical("capitalized");
+		t6.setPartOfSpeech("other");
+		assertEquals(0, ("other").compareToIgnoreCase(t6.getPartOfSpeech()));
+		
+		t7.setLexical("null");
+		t7.setPartOfSpeech("other");
+		assertEquals(0, ("other").compareToIgnoreCase(t7.getPartOfSpeech()));
+		
+		t8.setLexical("punct");
+		t8.setPartOfSpeech("comma");
+		assertEquals(0, ("comma").compareToIgnoreCase(t8.getPartOfSpeech()));
+		
+		t9.setLexical("punct");
+		t9.setPartOfSpeech("hyphen");
+		assertEquals(0, ("hyphen").compareToIgnoreCase(t9.getPartOfSpeech()));	
+	}
+	
+	@Test
+	/**
+	 * test for public void setPartOfSpeech(String partOfSpeech)
+	 */
+	public void testSetPartOfSpeech(){
+		/*
+		 * Parts of Speech
+
+				 - Articles (“a”, “an”, “the”)			
+				 - Conjunction, specifically “and”				
+				 - Period					
+				 - Comma				
+				 - Hyphen
+				 - other
+		 */
+		
+		Token t1 = new Token("an");
+		Token t2 = new Token(".");
+		Token t3 = new Token("and");
+		Token t4 = new Token("bathroom");
+		Token t5 = new Token("H");
+		Token t6 = new Token("Sally");
+		Token t7 = new Token();
+		Token t8 = new Token(",");
+		Token t9 = new Token("-");
+		
+		//t1.setLexical("other");
+		t1.setPartOfSpeech("article");
+		assertEquals("article", t1.getPartOfSpeech());
+		
+		//t2.setLexical("punct");
+		t2.setPartOfSpeech("period");
+		assertEquals("period", t2.getPartOfSpeech());
+		
+		//t3.setLexical("other");
+		t3.setPartOfSpeech("conjunction");
+		assertEquals("conjunction" , t3.getPartOfSpeech());
+		
+		//t4.setLexical("other");
+		t4.setPartOfSpeech("other");
+		assertEquals("other" , t4.getPartOfSpeech());
+		
+		//t5.setLexical("CapLetter");
+		t5.setPartOfSpeech("other");
+		assertEquals("other" , t5.getPartOfSpeech());
+		
+		//t6.setLexical("capitalized");
+		t6.setPartOfSpeech("other");
+		assertEquals("other" , t6.getPartOfSpeech());
+		
+		//t7.setLexical("null");
+		t7.setPartOfSpeech("other");
+		assertEquals("other" , t7.getPartOfSpeech());
+		
+		//t8.setLexical("punct");
+		t8.setPartOfSpeech("comma");
+		assertEquals("comma" , t8.getPartOfSpeech());
+		
+		//t9.setLexical("punct");
+		t9.setPartOfSpeech("hyphen");
+		assertEquals("hyphen" , t9.getPartOfSpeech());	
+	}
+	
+	
+	@Test
+	/**
+	 * test for public int isDictionaryWord()
+	 */
+	public void testIsDictionaryWord(){
+		Token t1 = new Token("desk");
+		Token t2 = new Token();
+		Token t3 = new Token("");
+		Token t4 = new Token("strawberry");
+		Token t5 = new Token("ODU");
+		
+		t1.setDictionaryWord(1);
+		t2.setDictionaryWord(0);
+		t3.setDictionaryWord(0);
+		t4.setDictionaryWord(1);
+		t5.setDictionaryWord(0);
+		assertEquals(1 , t1.isDictionaryWord());
+		assertEquals(0 , t2.isDictionaryWord());
+		assertEquals(0 , t3.isDictionaryWord());
+		assertEquals(1 , t4.isDictionaryWord());
+		assertEquals(0 , t5.isDictionaryWord());
+	}
+	
+	
+	@Test
+	/**
+	 * test for public void setDictionaryWord(int dictionaryWord) 
+	 */
+	public void testSetDictionaryWord(){
+		Token t1 = new Token("desk");
+		Token t2 = new Token();
+		Token t3 = new Token("");
+		Token t4 = new Token("strawberry");
+		Token t5 = new Token("ODU");
+		
+		t1.setDictionaryWord(1);
+		t2.setDictionaryWord(0);
+		t3.setDictionaryWord(0);
+		t4.setDictionaryWord(1);
+		t5.setDictionaryWord(0);
+		
+		assertEquals(1 , t1.isDictionaryWord());
+		assertEquals(0 , t2.isDictionaryWord());
+		assertEquals(0 , t3.isDictionaryWord());
+		assertEquals(1 , t4.isDictionaryWord());
+		assertEquals(0 , t5.isDictionaryWord());
 		
 	}
 	
 	
+	@Test
+	/**
+	 * test for public int isCityState()
+	 */
+	public void testIsCityState(){
+		Token vermont = new Token("Vermont"); //state
+		Token norfolk = new Token("Norfolk"); //city
+		Token vabeach = new Token("Virginia Beach"); //city
+		Token va = new Token("Virginia"); //state
+		Token sc = new Token("South Carolina"); //state
+		Token milford = new Token("Milford"); //city
+		Token ca = new Token("California"); //state
+		Token boston = new Token("Boston"); //city
+		Token shanghai = new Token("Shanghai"); //city
+		Token london = new Token("London"); //city
+		Token cairo = new Token("Cairo"); //city
+		
+		Token t1 = new Token("John");
+		Token t2 = new Token("Ashley");
+		Token t3 = new Token("desk");
+		Token t4 = new Token("lamp");
+		Token t5 = new Token("ocean");
+		
+		vermont.setCityState(1);
+		norfolk.setCityState(1);
+		vabeach.setCityState(1);
+		va.setCityState(1);
+		sc.setCityState(1);
+		milford.setCityState(1);
+		ca.setCityState(1);
+		boston.setCityState(1);
+		shanghai.setCityState(1);
+		london.setCityState(1);
+		cairo.setCityState(1);
+		t1.setCityState(0);
+		t2.setCityState(0);
+		t3.setCityState(0);
+		t4.setCityState(0);
+		t5.setCityState(0);
+		
+		assertEquals(1 , vermont.isCityState());
+		assertEquals(1 , norfolk.isCityState());
+		assertEquals(1 , vabeach.isCityState());
+		assertEquals(1 , va.isCityState());
+		assertEquals(1 , sc.isCityState());
+		assertEquals(1 , milford.isCityState());
+		assertEquals(1 , ca.isCityState());
+		assertEquals(1 , boston.isCityState());
+		assertEquals(1 , shanghai.isCityState());
+		assertEquals(1 , london.isCityState());
+		assertEquals(1 , cairo.isCityState());
+		assertEquals(0, t1.isCityState());
+		assertEquals(0, t2.isCityState());
+		assertEquals(0, t3.isCityState());
+		assertEquals(0, t4.isCityState());
+		assertEquals(0, t5.isCityState());
+	}
 	
 	
+	@Test
+	/**
+	 * test for public void setCityState(int cityState)
+	 */
+	public void testSetCityState(){
+		
+		Token vermont = new Token("Vermont"); //state
+		Token norfolk = new Token("Norfolk"); //city
+		Token vabeach = new Token("Virginia Beach"); //city
+		Token va = new Token("Virginia"); //state
+		Token sc = new Token("South Carolina"); //state
+		Token milford = new Token("Milford"); //city
+		Token ca = new Token("California"); //state
+		Token boston = new Token("Boston"); //city
+		
+		Token t1 = new Token("John");
+		Token t2 = new Token("Ashley");
+		Token t3 = new Token("desk");
+		Token t4 = new Token("lamp");
+		Token t5 = new Token("ocean");
+		
+		vermont.setCityState(1);
+		norfolk.setCityState(1);
+		vabeach.setCityState(1);
+		va.setCityState(1);
+		sc.setCityState(1);
+		milford.setCityState(1);
+		ca.setCityState(1);
+		boston.setCityState(1);
+		t1.setCityState(0);
+		t2.setCityState(0);
+		t3.setCityState(0);
+		t4.setCityState(0);
+		t5.setCityState(0);
+		
+		assertEquals(1 , vermont.isCityState());
+		assertEquals(1 , norfolk.isCityState());
+		assertEquals(1 , vabeach.isCityState());
+		assertEquals(1 , va.isCityState());
+		assertEquals(1 , sc.isCityState());
+		assertEquals(1 , milford.isCityState());
+		assertEquals(1 , ca.isCityState());
+		assertEquals(1 , boston.isCityState());
+		assertEquals(0, t1.isCityState());
+		assertEquals(0, t2.isCityState());
+		assertEquals(0, t3.isCityState());
+		assertEquals(0, t4.isCityState());
+		assertEquals(0, t5.isCityState());
+	
+	}
+	
+	@Test
+	/**
+	 * test for public int isCountryTerritory()
+	 */
+	public void testIsCountryTerritory(){
+		Token egypt = new Token("Egypt");
+		Token canada = new Token("Canada");
+		Token southafrica = new Token("South Africa");
+		Token china = new Token("China");
+		Token france = new Token("France");
+		
+		Token t1 = new Token("John");
+		Token t2 = new Token("Ashley");
+		Token t3 = new Token("desk");
+		Token t4 = new Token("lamp");
+		Token t5 = new Token("ocean");
+		
+		egypt.setCountryTerritory(1);
+		canada.setCountryTerritory(1);
+		southafrica.setCountryTerritory(1);
+		china.setCountryTerritory(1);
+		france.setCountryTerritory(1);
+		t1.setCountryTerritory(0);
+		t2.setCountryTerritory(0);
+		t3.setCountryTerritory(0);
+		t4.setCountryTerritory(0);
+		t5.setCountryTerritory(0);
+		
+		assertEquals(1 , egypt.isCountryTerritory());
+		assertEquals(1 , canada.isCountryTerritory());
+		assertEquals(1 , china.isCountryTerritory());
+		assertEquals(1 , southafrica.isCountryTerritory());
+		assertEquals(1 , france.isCountryTerritory());
+		assertFalse(t1.isCountryTerritory() == 1);
+		assertFalse(t2.isCountryTerritory() == 1);
+		assertFalse(t3.isCountryTerritory() == 1);
+		assertFalse(t4.isCountryTerritory() == 1);
+		assertFalse(t5.isCountryTerritory() == 1);
+	}
 	
 	
-	
+	@Test
+	/**
+	 * test for public void setCountryTerritory(int countryTerritory)
+	 */
+	public void testSetCountryTerritory(){
+		
+		Token egypt = new Token("Egypt");
+		Token canada = new Token("Canada");
+		Token southafrica = new Token("South Africa");
+		Token china = new Token("China");
+		Token france = new Token("France");
+		
+		Token t1 = new Token("John");
+		Token t2 = new Token("Ashley");
+		Token t3 = new Token("desk");
+		Token t4 = new Token("lamp");
+		Token t5 = new Token("ocean");
+		
+		egypt.setCountryTerritory(1);
+		canada.setCountryTerritory(1);
+		southafrica.setCountryTerritory(1);
+		china.setCountryTerritory(1);
+		france.setCountryTerritory(1);
+		t1.setCountryTerritory(0);
+		t2.setCountryTerritory(0);
+		t3.setCountryTerritory(0);
+		t4.setCountryTerritory(0);
+		t5.setCountryTerritory(0);
+		
+		assertEquals(1 , egypt.isCountryTerritory());
+		assertEquals(1 , canada.isCountryTerritory());
+		assertEquals(1 , china.isCountryTerritory());
+		assertEquals(1 , southafrica.isCountryTerritory());
+		assertEquals(1 , france.isCountryTerritory());
+		assertEquals(0 , t1.isCountryTerritory());
+		assertEquals(0 , t2.isCountryTerritory());
+		assertEquals(0 , t3.isCountryTerritory());
+		assertEquals(0 , t4.isCountryTerritory());
+		assertEquals(0 , t5.isCountryTerritory());
+	}
 	
 	// User Story #850
 	// Status - Completed
