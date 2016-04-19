861b7a07dd014c89bd138f08a1a90f000112eefe
Tristan Pressley <tpres008@odu.edu>
2016-04-18 20:00:38 -0400

    Updated TestSystem.java

--- src/test/java/edu/odu/cs/cs350/namex/TestSystem.java
+++ src/test/java/edu/odu/cs/cs350/namex/TestSystem.java
@@ -2,7 +2,6 @@
 package edu.odu.cs.cs350.namex;
 
 import static org.junit.Assert.*;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -21,24 +20,23 @@ public class TestSystem {
 	@Before
 	public void setUp() throws Exception {
 	}
-
 	@Test
 	public void testMainTrainData() {
-		String[] args = { "train", "src/main/data/trainingData.txt", "trainingDatao.txt" };
+		String[] args = { "train", "src/main/data/trainingDataSmall.txt", "trainingDatao.txt" };
 		try {
 			// Temporarily removed -- takes way too long
-			// Librarian.main(args);
+			Librarian.main(args);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		File f = new File("LearningMachine.model");
 		File f2 = new File("trainigDatao.txt");
 		Scanner s = null;
-		try {
+		/*try {
 			s = new Scanner(f2);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
+		}*/
 		assertTrue(f.exists());
 		assertTrue(s.nextLine() == "@relation Classification");
 
